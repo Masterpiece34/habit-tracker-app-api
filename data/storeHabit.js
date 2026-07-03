93266ae4587e1b6725d16f2230ce90d061d3ce27
emicum <alliuemmanuel101@gmail.com>
2026-07-03 04:50:22 +0100

    developed a function that holds data coming from habits.json so that it can be called anytime

--- data/storeHabit.js
+++ data/storeHabit.js
@@ -5,3 +5,8 @@ import { fileURLToPath } from "url";
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const DATA_FILE = path.join(__dirname, "habit.json");
+
+function readHabitFile() {
+  const raw = fs.readFileSync(DATA_FILE, "utf-8");
+  return JSON.parse(raw);
+}
